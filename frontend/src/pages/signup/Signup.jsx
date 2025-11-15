e4dff11e3d40bc708684b24d8edddbd2a7809f33
Anish-649 <kumaranish4098@gmail.com>
2025-11-15 19:44:59 +0530

    m

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
-import "./Signup.css";
+import axios from "axios"
+import "./signup.css";
 
 const CustomerSignup = () => {
   const [form, setForm] = useState({
